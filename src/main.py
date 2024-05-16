94829d72a7995949ccee0448f6e5095e78193409
Gheorghe Luța <georgelutaoff@gmail.com>
2024-05-16 23:01:39 +0300

    feat : first commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import regine_algoritm_genetic.my_module as regine_algoritm_genetic
+
+def main():
+    print(regine_algoritm_genetic.add(1 ,1))
+
+if __name__ == '__main__':
+    main()
---
+++ src/regine_algoritm_genetic/my_module.py
@@ -0,0 +1,7 @@
+# Acesta este un modul simplu în Python
+
+def hello_world():
+    print("Hello, world!")
+
+def add(a, b):
+    return a + b
